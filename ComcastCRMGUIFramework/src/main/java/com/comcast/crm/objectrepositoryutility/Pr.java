bfd467e4f2d3c9dbe5338fc486eb6adbee2aebb9
Nandha GitHub <Nandhakumar.d@nandha.net>
2024-07-17 12:45:20 +0530

    8

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Pr.java
@@ -0,0 +1,12 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Pr {
+	@FindBy(xpath = "//img [@alt='Create Product...']")
+	private WebElement createproductImgbtn;
+
+	//welcoe
+
+}
